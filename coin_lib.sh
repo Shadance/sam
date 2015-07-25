4b231d5f68dba7c94a74a39291201a1b7ae49e5d
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2015-07-25 12:02:09 -0400

    Migrate to latest coin

--- coin_lib.sh
+++ coin_lib.sh
@@ -9,9 +9,9 @@ fi
 
 cd lib
 
-coin https://pypi.python.org/packages/py2/u/urllib3/urllib3-1.10.4-py2-none-any.whl
-coin https://pypi.python.org/packages/source/j/jsonpickle/jsonpickle-0.9.2.tar.gz
-coin https://pypi.python.org/packages/source/c/convertible/convertible-0.13.tar.gz
-coin https://pypi.python.org/packages/source/s/syncloud-app/syncloud-app-0.38.tar.gz
+coin py https://pypi.python.org/packages/py2/u/urllib3/urllib3-1.10.4-py2-none-any.whl
+coin py https://pypi.python.org/packages/source/j/jsonpickle/jsonpickle-0.9.2.tar.gz
+coin py https://pypi.python.org/packages/source/c/convertible/convertible-0.13.tar.gz
+coin py https://pypi.python.org/packages/source/s/syncloud-app/syncloud-app-0.38.tar.gz
 
 cd ..
